411a8589af68c0b2c0504f2e88efb248d65b94e4
Yintong <zhouyintong96@gmail.com>
2019-08-13 13:14:39 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+$("div").css("background","purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border","2px solid orange");
+$("div:first-of-type").css("color","pink")
+
+////////////////////////
+var text = $("h1").text()
+console.log(text)
+
+$("img").css("width","400px")
+$("img").attr("src","./tiger2.jpg")
+$("input").attr("type","color")
